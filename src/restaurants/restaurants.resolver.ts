2d5fb6384d94d705a60650e55edf1f77743d5fa3
Max16 <max16@naver.com>
2021-07-04 08:58:26 +0900

    #1.3 Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver() // decorator 사용으로 graphql schema 작성 할 필요 없음
+@Resolver(of => Restaurant) // decorator 사용으로 graphql schema 작성 할 필요 없음
 export class ResturantResolver {
-  @Query(returns => Restaurant)
-  myResturant() {
-    return true;
+  @Query(returns => [Restaurant])
+  resturant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
   }
 }
