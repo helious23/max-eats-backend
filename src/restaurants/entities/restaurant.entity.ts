dd0e7d54a2d323de67856a8e3d46a042d8d58047
Max16 <max16@naver.com>
2021-07-04 09:18:35 +0900

    #1.4 InputTypes and ArgumentsTypes

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -6,5 +6,9 @@ export class Restaurant {
   @Field(type => String) // as a first argument, ReturnFunction required
   name: string;
   @Field(type => Boolean, { nullable: true }) // nullable : optional
-  isGood?: boolean;
+  isVegan?: boolean;
+  @Field(type => String)
+  address: string;
+  @Field(type => String)
+  ownerName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,16 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateResturantDto } from './dtos/create-resturant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant) // decorator 사용으로 graphql schema 작성 할 필요 없음
 export class ResturantResolver {
   @Query(returns => [Restaurant])
   resturant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
     return [];
+  }
+  @Mutation(retuns => Boolean)
+  createResturant(@Args() createResturantDto: CreateResturantDto): boolean {
+    console.log(createResturantDto);
+    return true;
   }
 }
