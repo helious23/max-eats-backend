59bf6d6e94f712f2d53352970531d129f80ec65a
Max16 <max16@naver.com>
2021-07-05 15:34:47 +0900

    #2.3 TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'max16',
+      password: 'test',
+      database: 'max-eats',
+      synchronize: true,
+      logging: true,
+    }),
+    RestaurantsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: true, // schemaFile 메모리에 저장
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
