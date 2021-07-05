d705d425e4b750cd70bc03f77e3c39fcf45c24c3
Max16 <max16@naver.com>
2021-07-05 16:28:29 +0900

    #3.0 Our First Entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,14 +1,31 @@
 // Query typeDef for GraphQL
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType() // for graphQL schema
+@Entity() // for typeORM for binding to DB
 export class Restaurant {
+  @Field(type => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field(type => String) // as a first argument, ReturnFunction required
+  @Column()
   name: string;
+
   @Field(type => Boolean, { nullable: true }) // nullable : optional
+  @Column()
   isVegan?: boolean;
+
   @Field(type => String)
+  @Column()
   address: string;
+
   @Field(type => String)
+  @Column()
   ownerName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryName: string;
 }
