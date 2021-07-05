338b225974367f8864fc26a1ae346064a56ed6af
Max16 <max16@naver.com>
2021-07-05 15:42:48 +0900

    #2.4 Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -15,10 +20,10 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       synchronize: true,
       logging: true,
     }),
-    RestaurantsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: true, // schemaFile 메모리에 저장
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
