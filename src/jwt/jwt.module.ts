53ffd24670d4b435910aaae1a6fec5031db00507
Max16 <max16@naver.com>
2021-07-07 13:07:09 +0900

    #5.4 JWT Module part Two

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,14 +1,22 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
 import { JwtService } from './jwt.service';
+import { CONFIG_OPTIONS } from './jwt.constants';
 
 @Module({})
 @Global() // globally 사용 가능하게 함
 export class JwtModule {
-  static forRoot(): DynamicModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
     return {
       module: JwtModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS, // jwtservice 에서 inject 할 이름
+          useValue: options, // 내용
+        },
+        JwtService,
+      ],
       exports: [JwtService],
-      providers: [JwtService],
     };
   }
 }
--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { CONFIG_OPTIONS } from './jwt.constants';
 
 @Injectable()
 export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions, // jwt.modules 에서 provide 로 받은 이름, 내용
+  ) {
+    console.log(options);
+  }
   hello() {
     console.log('hello');
   }
