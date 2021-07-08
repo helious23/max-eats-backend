ebb51312f94b6fecaca681d90eaaee7a622af4aa
Max16 <max16@naver.com>
2021-07-09 07:57:35 +0900

    #6.7 Mailgun API

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,3 +1,5 @@
+import got from 'got';
+import * as FormData from 'form-data';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from '../common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
@@ -6,7 +8,28 @@ import { MailModuleOptions } from './mail.interfaces';
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {}
+  ) {
+    this.sendEmail('testing', 'test', 'max16@naver.com');
+  }
 
-  private sendEmail(subject: string, content: string) {}
+  private async sendEmail(subject: string, content: string, to: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', to);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        method: 'POST',
+        body: form,
+      },
+    );
+    console.log(response.body);
+  }
 }
