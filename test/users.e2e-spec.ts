e1b27a65ddda2dff33e2fa179227b16a5f4bcd0c
Max16 <max16@naver.com>
2021-07-12 14:35:27 +0900

    #9.6 Testing me

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -236,7 +236,60 @@ describe('UserModule (e2e)', () => {
     });
   });
 
-  it.todo('me');
+  describe('me', () => {
+    it('should find my profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+            {
+              me{
+                email
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: {
+                me: { email },
+              },
+            },
+          } = res;
+          expect(email).toBe(testUser.email);
+        });
+    });
+
+    it('should not allow logged out user', () => {
+      return (
+        request(app.getHttpServer())
+          .post(GRAPHQL_ENDPOINT)
+          // .set('X-JWT', jwtToken)
+          .send({
+            query: `
+            {
+              me{
+                email
+              }
+            }
+          `,
+          })
+          .expect(200)
+          .expect(res => {
+            const {
+              body: {
+                errors: [{ message }],
+              },
+            } = res;
+            expect(message).toBe('Forbidden resource');
+          })
+      );
+    });
+  });
+
   it.todo('verifyEmail');
   it.todo('editProfile');
 });
