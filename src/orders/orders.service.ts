910fa86a7ece6b07f7af1b81ac45935af0b3a8bc
Max16 <max16@naver.com>
2021-07-15 16:59:41 +0900

    #11.7 Create Order part Two

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,11 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { User } from '../users/entities/user.entity';
+import { Restaurant } from '../restaurants/entities/restaurant.entity';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
   ) {}
+
+  async createOrder(
+    customer: User,
+    { resturantId, items }: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(resturantId);
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: '식당을 찾지 못했습니다',
+        };
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error: '주문을 만들지 못했습니다',
+      };
+    }
+  }
 }
