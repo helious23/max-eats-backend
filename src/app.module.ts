c9dcdeaa49af1b0f9e2b1d9cfd5025afbf0f4413
Max16 <max16@naver.com>
2021-07-18 11:38:48 +0900

    #13.2 Payment Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,8 @@ import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
 import { CommonModule } from './common/common.module';
+import { PaymentsModule } from './payments/payments.module';
+import { Payment } from './payments/entities/payment.entity';
 
 @Module({
   imports: [
@@ -62,6 +64,7 @@ import { CommonModule } from './common/common.module';
         Dish,
         Order,
         OrderItem,
+        Payment,
       ], // going to DB(table)
     }),
     GraphQLModule.forRoot({
@@ -89,6 +92,7 @@ import { CommonModule } from './common/common.module';
     RestaurantsModule,
     OrdersModule,
     CommonModule,
+    PaymentsModule,
   ],
   controllers: [],
   providers: [],
