078f9cfa4d23ec847404ca4f2cf38d1be9bd0284
Max16 <max16@naver.com>
2021-08-04 09:48:13 +0900

    myRestaurant relations menu

--- src/restaurants/resturants.service.ts
+++ src/restaurants/resturants.service.ts
@@ -100,7 +100,10 @@ export class RestaurantService {
     { id }: MyRestaurantInput,
   ): Promise<MyRestaurantOutput> {
     try {
-      const restaurant = await this.restaurants.findOne({ owner, id });
+      const restaurant = await this.restaurants.findOne(
+        { owner, id },
+        { relations: ['menu'] },
+      );
       return {
         ok: true,
         restaurant,
