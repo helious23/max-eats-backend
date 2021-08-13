dd70b83bf4b5d8003d92b5f529a4db24f95f86e7
Max16 <max16@naver.com>
2021-08-13 20:33:05 +0900

    reset

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,15 +27,15 @@ import { UploadsModule } from './uploads/uploads.module';
     ConfigModule.forRoot({
       isGlobal: true, // globally env file에 접근 : 필요한 module 의 import 에 ConfigService 로 접근
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'production', // product 시 env 파일 무시
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // product 시 env 파일 무시
       validationSchema: Joi.object({
         // env 파일 validation : 내용이 부족하면 app 실행 X
-        NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(),
-        DB_HOST: Joi.string(), // prod 일 때 url 사용하므로 required 삭제
-        DB_PORT: Joi.string(),
-        DB_USERNAME: Joi.string(),
-        DB_PASSWORD: Joi.string(),
-        DB_NAME: Joi.string(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
         PRIVATE_KEY: Joi.string().required(),
         MAINGUN_API_KEY: Joi.string().required(),
         MAILGUN_FROM_EMAIL: Joi.string().required(),
@@ -44,20 +44,14 @@ import { UploadsModule } from './uploads/uploads.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      ...(process.env.DATABASE_URL
-        ? { url: process.env.DATABASE_URL } // url 에 정보가 계속 바뀜
-        : {
-            host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD, // not required on localhost
-            database: process.env.DB_NAME,
-          }),
-
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD, // not required on localhost
+      database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod', // auto migration. production 시에는 false
       logging:
-        process.env.NODE_ENV !== 'production' &&
-        process.env.NODE_ENV !== 'test', // check log. production 시에는 false
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test', // check log. production 시에는 false
       entities: [
         User,
         Verification,
@@ -70,7 +64,6 @@ import { UploadsModule } from './uploads/uploads.module';
       ], // going to DB(table)
     }),
     GraphQLModule.forRoot({
-      playground: process.env.NODE_ENV !== 'production', // production 에서는 playground 실행 X
       installSubscriptionHandlers: true, // 서버가 웹소켓 기능을 가지게 됨
       autoSchemaFile: true, // schemaFile 메모리에 저장
       context: ({ req, connection }) => {
